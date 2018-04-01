78f6c08d756ed8bda8096c3d3a72787ff8791a2c
AaronW <aaronchristoph.williams@uoit.net>
2018-04-01 03:00:15 -0400

    moved to profiler package

--- src/main/java/profiler/ReadWriter.java
+++ src/main/java/profiler/ReadWriter.java
@@ -1,4 +1,4 @@
-package java;
+package profiler;
 
 import java.io.*;
 import java.util.Scanner;
---
+++ src/main/java/profiler/Waifu.java
@@ -0,0 +1,64 @@
+package profiler;
+
+
+import javafx.scene.image.Image;
+
+public class Waifu {
+
+    private Image waifuImage;
+    private String waifuImageURL;
+    private String waifuName;
+    private double waifuRating;
+    private String waifuYESNO;
+
+    public Waifu(String url, String wname){
+        Image temp = new Image(url);
+        this.waifuImageURL = url;
+        this.waifuName = wname;
+        setWaifuImage(temp);
+        setWaifuImageURL(url);
+        setWaifuName(wname);
+    }
+
+    public void setWaifuImage(Image wi){
+
+        this.waifuImage = wi;
+    }
+    public Image getWaifuImage(){
+
+        return this.waifuImage;
+    }
+
+    public void setWaifuImageURL(String waifuURL){
+
+        this.waifuImageURL = waifuURL;
+    }
+
+    public String getWaifuImageURL() {
+        return waifuImageURL;
+    }
+
+    public void setWaifuName(String name){
+        this.waifuName = name;
+    }
+
+    public String getWaifuName() {
+        return waifuName;
+    }
+
+    public void setWaifuRating(double rate){
+        this.waifuRating = rate;
+    }
+
+    public double getWaifuRating() {
+        return waifuRating;
+    }
+
+    public void setWaifuYESNO(String waifuYESNO) {
+        this.waifuYESNO = waifuYESNO;
+    }
+
+    public String getWaifuYESNO() {
+        return waifuYESNO;
+    }
+}
