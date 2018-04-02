1c0cc9c0e3a77c19208c9f768be2d06320531628
A2Williams <aaronchristoph.williams@uoit.net>
2018-04-02 18:44:24 -0400

    initial branch commit

---
+++ src/main/java/profiler/ClientConnect.java
@@ -0,0 +1,8 @@
+package profiler;
+
+public class ClientConnect {
+    private final int PORT = 49000;
+    public ClientConnect(String hostName) {
+
+    }
+}
--- src/main/java/profiler/Main.java
+++ src/main/java/profiler/Main.java
@@ -10,17 +10,27 @@ import java.util.*;
 
 
 public class Main extends Application {
+    protected ClientConnect client;
     @Override
     public void start(Stage stage) throws Exception{
+        client = new ClientConnect(this.getParameters().toString());
         FXMLLoader loader = new FXMLLoader(getClass().getResource("display.fxml"));
         Parent root = loader.load();
-        Scene scene = new Scene(root, 600, 800);
-        stage.setTitle("TEST");
+        Scene scene = new Scene(root, 600, 600);
+        stage.setTitle("Waifu Profiler");
         stage.setScene(scene);
-        stage.setResizable(false);
+        stage.setResizable(true);
         stage.show();
     }
 
 
-    public static void main(String[] args) { launch(args); }
+    public static void main(String[] args) {
+        if (1 == args.length) {
+            launch(args);
+        } else {
+            System.err.println("ERROR: few arguments usage: " +
+                    "javac Main.java <hostname>");
+            System.exit(-1);
+        }
+    }
 }
