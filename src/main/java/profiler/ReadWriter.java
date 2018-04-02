d99e1bacbdb25aadc40d96617847dda0d7cbdba8
Kelldav <brandon.angod@uoit.net>
2018-04-02 17:00:08 -0400

    Add files via upload

---
+++ src/main/java/profiler/ReadWriter.java
@@ -0,0 +1,68 @@
+package profiler;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadWriter {
+    public static void ReadWriter(){}
+
+    public static void recordTag(String inp) {
+        File file = new File("pref.txt");
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file,true));
+            bw.write(inp);
+            bw.newLine();
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //Don't know if this is neccesary
+    public static void saveWaifu(Double ratinginp,String nameinp){
+        File file = new File("bestgirls.txt");
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file,true));
+            bw.write(ratinginp+","+nameinp);
+            bw.newLine();
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static boolean scanForTags(String requestedWord){
+        File file = new File("pref.txt");
+        try {
+            Scanner sc=new Scanner(file);
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.equals(requestedWord)) {
+                    return true;
+                }
+            }
+            sc.close();
+            return false;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+     public static boolean scanForWaifu(String requestedWord){
+        File file = new File("bestgirls.txt");
+        try {
+            Scanner sc=new Scanner(file);
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.equals(requestedWord)) {
+                    return true;
+                }
+            }
+            sc.close();
+            return false;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
