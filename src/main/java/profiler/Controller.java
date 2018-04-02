631dd5cfecd2e5f5579e4e8b23bfb6a68097a994
Kelldav <brandon.angod@uoit.net>
2018-04-02 16:23:29 -0500

    Update Controller.java
    Fixed Declaration of ReadWriter's position

--- src/main/java/profiler/Controller.java
+++ src/main/java/profiler/Controller.java
@@ -49,6 +49,7 @@ public class Controller {
         getRandomWaifu();
     }
     public void noClick(ActionEvent e) {
+        ReadWriter rw=new ReadWriter();
         waifuList.get(randNum).setWaifuYESNO("NO");
         double rate = 0.0;
         waifuList.get(randNum).setWaifuRating(rate);
@@ -56,7 +57,7 @@ public class Controller {
         waifuList.remove(randNum);
         if(waifuList.size() == 0){
             for(int k = 0; k < tempList.size(); k++){
-                ReadWriter rw=new ReadWriter();
+                
                 rw.saveWaifu(tempList.get(k).getWaifuRating(),tempList.get(k).getWaifuName());
                 System.out.println(
                         tempList.get(k).getWaifuName() + " - " + " RATE: " + tempList.get(k).getWaifuRating() + ". Is Waifu: "
